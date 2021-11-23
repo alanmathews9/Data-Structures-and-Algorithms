49492693e06e966025fb74407bfaf2bf3f1e6cf6
Achyuth Mohan <achyuthmohan1@gmail.com>
2021-11-23 20:42:16 +0530

    added new contents

--- conditionals loops and calculator program/upperorlower.java
+++ conditionals loops and calculator program/upperorlower.java
@@ -6,8 +6,9 @@ public class upperorlower {
 
         System.out.println(input.next().trim()); //during run time we can enter a string  and this will remove all the white spaces form the string and will print the rest.
                                                  //eg:Achyuth Mohan output:-Achyuth
+        String word="HEllo";
+        System.out.println(word.charAt(2));// this will print the character at index 2
     }                                            
 }
 
 
-video conditional loops : 30:00min
