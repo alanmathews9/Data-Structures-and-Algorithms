73dfffb1b773eb681506163ae51d7ba84a6a1bef
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-11-22 06:35:23 +0530

    tree traversals

--- Java/BinaryTree/BinaryTree.java
+++ Java/BinaryTree/BinaryTree.java
@@ -23,12 +23,45 @@ public class BinaryTree{
             return newnode;
 
         }
+        public static void preOrder(Node root){
+            if(root==null){
+                return ;
+            }
+            System.out.print(root.data+" ");
+            preOrder(root.left);
+            preOrder(root.right);
+            
+        }
+        public static void inOrder(Node root){
+            if(root==null){
+                return ;
+            }
+            inOrder(root.left);
+            System.out.print(root.data+" ");
+            inOrder(root.right);
+            
+        }
+        public static void postOrder(Node root){
+            if(root==null){
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data+" ");
+        }
     }
     public static void main(String[] args) {
       Bt tree=new Bt();
       int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
       Node root=tree.buildtree(nodes);
       System.out.println(root.data);
-
+      System.out.println("Pre order traversal: ");
+      tree.preOrder(root);
+      System.out.println();
+      System.out.println("In order traversal: ");
+      tree.inOrder(root);
+      System.out.println();
+      System.out.println("Post order traversal: ");
+      tree.postOrder(root);
     }
 }
